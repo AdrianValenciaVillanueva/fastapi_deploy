a45d0f98f2f448930815c4cb2d694a4ea41b6341
AdrianValenciaVillanueva <157079095+AdrianValenciaVillanueva@users.noreply.github.com>
2024-05-09 10:02:25 -0600

    Update client.py

--- database/client.py
+++ database/client.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import gridfs #libreria para trabajar con archivos en mongoDB por encima de 16MB
 
-db_client = MongoClient("mongodb+srv://AdrianValvi:Adrian2004.@cluster0.tz1cgit.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0")
+db_client = MongoClient("mongodb+srv://AdrianValvi:prograweb@cluster0.tz1cgit.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0")
 
 db = db_client.test #conecta a la base de datos test
 fs = gridfs.GridFS(db) #pasar la base de datos a la gridfs
